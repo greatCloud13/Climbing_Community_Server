cface68091f2ea756b2f2141363f47d8e214cef6
greatCloud13 <138741201+greatCloud13@users.noreply.github.com>
2026-01-14 21:23:37 +0900

    init Commit

---
+++ src/main/java/com/project/greatcloud13/ClimbingWith/ClimbingWithApplication.java
@@ -0,0 +1,13 @@
+package com.project.greatcloud13.ClimbingWith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimbingWithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimbingWithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/greatcloud13/ClimbingWith/ClimbingWithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.greatcloud13.ClimbingWith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimbingWithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
