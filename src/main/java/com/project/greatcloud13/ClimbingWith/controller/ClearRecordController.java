c6aa40212ddaec54028f4371dd44962b662e934f
greatCloud13 <138741201+greatCloud13@users.noreply.github.com>
2026-03-11 20:24:22 +0900

    1. ClearRecordController 구현

---
+++ src/main/java/com/project/greatcloud13/ClimbingWith/controller/ClearRecordController.java
@@ -0,0 +1,91 @@
+package com.project.greatcloud13.ClimbingWith.controller;
+
+import com.project.greatcloud13.ClimbingWith.dto.ClearRecordCreateDTO;
+import com.project.greatcloud13.ClimbingWith.dto.ClearRecordResponseDTO;
+import com.project.greatcloud13.ClimbingWith.dto.ClearRecordSummaryDTO;
+import com.project.greatcloud13.ClimbingWith.dto.ClearRecordUpdateDTO;
+import com.project.greatcloud13.ClimbingWith.entity.ClearRecord;
+import com.project.greatcloud13.ClimbingWith.service.ClearRecordService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/clearRecord")
+@RequiredArgsConstructor
+public class ClearRecordController {
+
+    private final ClearRecordService clearRecordService;
+
+    @Operation(
+            summary = "완등 기록 작성",
+            description = "새로운 완등기록을 작성합니다."
+    )
+    @PostMapping
+    public ResponseEntity<ClearRecordResponseDTO> createClearRecord(@RequestBody ClearRecordCreateDTO request){
+        ClearRecordResponseDTO result = clearRecordService.createClearRecord(request);
+
+        return ResponseEntity.ok(result);
+    }
+
+    @Operation(
+            summary = "사용자 ID 기준 완등 기록 조회",
+            description = "최신 일자의 완등 기록부터 정렬됩니다"
+    )
+    @GetMapping("/page/{page}/size/{size}")
+    public ResponseEntity<Page<ClearRecordSummaryDTO>> getAllClearRecordSummaryByUserId(
+            @PathVariable Long userId, @PathVariable int page, @PathVariable int size){
+        Page<ClearRecordSummaryDTO> result = clearRecordService.getClearRecordSummaryByUserId(userId, page, size);
+
+        return ResponseEntity.ok(result);
+    }
+
+    @Operation(
+            summary = "문제 ID에 해당하는 완등 기록 조회",
+            description = "문제 ID를 기준으로 영상 URL이 존재하는 완등 기록을 조회합니다."
+    )
+    @GetMapping("/existVideo/problem/{problemId}/page/{page}/size/{size}")
+    public ResponseEntity<Page<ClearRecordSummaryDTO>> getExistVideoClearRecordSummaryByProblem(
+            @PathVariable Long problemId, @PathVariable int page, @PathVariable int size){
+        Page<ClearRecordSummaryDTO> result = clearRecordService.getClearRecordSummaryByProblemExistVideoUrl(problemId, page, size);
+
+        return ResponseEntity.ok(result);
+    }
+
+    @Operation(
+            summary = "섹터 ID에 해당하는 완등 기록 조회",
+            description = "섹터 ID를 기준으로 영상 URL이 존재하는 완등 기록을 조회합니다."
+    )
+    @GetMapping("/existVideo/sector/{sectorId}/page/{page}/size/{size}")
+    public ResponseEntity<Page<ClearRecordSummaryDTO>> getClearRecordSummaryBySectorExistVideoUrl(
+            @PathVariable Long sectorId,@PathVariable int page,@PathVariable int size){
+        Page<ClearRecordSummaryDTO> result = clearRecordService.getClearRecordSummaryBySectorExistVideoUrl(sectorId, page, size);
+
+        return ResponseEntity.ok(result);
+    }
+
+    @Operation(
+            summary = "완등 기록 업데이트",
+            description = "기존의 완등기록을 갱신합니다."
+    )
+    @PutMapping("/{id}")
+    public ResponseEntity<ClearRecordResponseDTO> updateClearRecord(@PathVariable Long id, @RequestBody ClearRecordUpdateDTO request){
+        ClearRecordResponseDTO result = clearRecordService.updateClearRecord(id, request);
+
+        return ResponseEntity.ok(result);
+    }
+
+    @Operation(
+            summary = "완등 기록 삭제",
+            description = "ID에 해당하는 완등 기록을 영구적으로 삭제합니다."
+    )
+    @PutMapping
+    public ResponseEntity<Boolean> deleteRecord(Long id){
+        clearRecordService.deleteClearRecord(id);
+
+        return ResponseEntity.ok(true);
+    }
+
+}
