d0bda8180c3db69b2c6ab36b1673bc78fef042d3
greatCloud13 <138741201+greatCloud13@users.noreply.github.com>
2026-03-25 15:52:39 +0900

    FIX: URL 권한 임시 해제 차후 재설정 필요, swaggerConfig 작성

--- src/main/java/com/project/greatcloud13/ClimbingWith/config/SecurityConfig.java
+++ src/main/java/com/project/greatcloud13/ClimbingWith/config/SecurityConfig.java
@@ -60,6 +60,7 @@ public class SecurityConfig {
                 //URL 권한 설정
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll() // 인증 관련 API는 모두 허용
+                        .requestMatchers("/api/**").permitAll()         // #todo 권한별 api 접근 설정 필요
                         .requestMatchers("/swagger-ui/**", "/api-docs/**", "/v3/api-docs/**").permitAll() //Swagger api 허용
                         .requestMatchers("/error/**").permitAll()
                         .anyRequest().authenticated())
