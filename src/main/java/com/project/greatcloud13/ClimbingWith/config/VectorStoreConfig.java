d770c31387a38d5b42b25e10c50e532973f87916
greatCloud13 <138741201+greatCloud13@users.noreply.github.com>
2026-04-17 20:07:32 +0900

    FEAT: EmbeddingStore MetadataKey 추가 및 PostRepository streamAllBy 메소드 추가

--- src/main/java/com/project/greatcloud13/ClimbingWith/config/VectorStoreConfig.java
+++ src/main/java/com/project/greatcloud13/ClimbingWith/config/VectorStoreConfig.java
@@ -6,17 +6,18 @@ import dev.langchain4j.store.embedding.redis.RedisEmbeddingStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class VectorStoreConfig {
-
     @Bean
     public EmbeddingStore<TextSegment> embeddingStore(){
         return RedisEmbeddingStore.builder()
                 .host("vector")
                 .port(6379)
                 .dimension(1024) //bge-m3 차원
+                .metadataKeys(List.of("postId"))
                 .indexName("climbing-knowledge-Index")
                 .build();
     }
-
 }
