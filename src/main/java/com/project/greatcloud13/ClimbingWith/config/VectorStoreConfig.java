1ad50953992296226b55c67c37f4f5438903e18f
greatCloud13 <138741201+greatCloud13@users.noreply.github.com>
2026-07-17 16:54:38 +0900

    REFACTOR: 하드코딩된 host 및 port 설정파일로 분리

--- src/main/java/com/project/greatcloud13/ClimbingWith/config/VectorStoreConfig.java
+++ src/main/java/com/project/greatcloud13/ClimbingWith/config/VectorStoreConfig.java
@@ -3,6 +3,7 @@ package com.project.greatcloud13.ClimbingWith.config;
 import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.store.embedding.EmbeddingStore;
 import dev.langchain4j.store.embedding.redis.RedisEmbeddingStore;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,10 +12,12 @@ import java.util.List;
 @Configuration
 public class VectorStoreConfig {
     @Bean
-    public EmbeddingStore<TextSegment> embeddingStore(){
+    public EmbeddingStore<TextSegment> embeddingStore(
+            @Value("${vector.redis.host}") String host,
+            @Value("${vector.redis.port}") int port) {
         return RedisEmbeddingStore.builder()
-                .host("vector")
-                .port(6379)
+                .host(host)
+                .port(port)
                 .dimension(1024) //bge-m3 차원
                 .metadataKeys(List.of("postId"))
                 .indexName("climbing-knowledge-Index")
