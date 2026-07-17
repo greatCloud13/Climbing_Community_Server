d57990607bb438630798c87a6acdca64c68d87f9
greatCloud13 <138741201+greatCloud13@users.noreply.github.com>
2026-07-17 18:18:36 +0900

    BUG: PostType 클래스 신뢰 리스트 추가, Jackson 메시지 컨버터 추가로 필요없어진 PostMessage Serializable 인터페이스 제거, Json 큐 무한 재시도 방지를 위한 3회 시도 실패시 DLQ로 격리 로직 추가

--- src/main/java/com/project/greatcloud13/ClimbingWith/dto/PostMessage.java
+++ src/main/java/com/project/greatcloud13/ClimbingWith/dto/PostMessage.java
@@ -6,12 +6,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.io.Serializable;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class PostMessage implements Serializable {
+public class PostMessage {
 
     private String gymName;
     private String postTitle;
