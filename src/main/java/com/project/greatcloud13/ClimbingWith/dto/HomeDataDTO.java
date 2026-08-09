5c576f3655fda34c3d8a911e43145f218d52090a
greatCloud13 <138741201+greatCloud13@users.noreply.github.com>
2026-08-09 15:08:37 +0900

    FEAT: 암장 홈화면 api 추가

---
+++ src/main/java/com/project/greatcloud13/ClimbingWith/dto/HomeDataDTO.java
@@ -0,0 +1,47 @@
+package com.project.greatcloud13.ClimbingWith.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HomeDataDTO {
+
+    private List<GymCard> gymCardList;
+
+    @Data
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class GymCard {
+
+        private Long gymId;
+
+        private String gymName;
+
+        private String address;
+
+        private String imageUrl;
+
+        private List<GymNotice> notices;
+    }
+
+    @Data
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class GymNotice {
+
+        private Long postId;
+
+        private String noticeTitle;
+
+        private String date;
+    }
+}
