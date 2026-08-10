460d215c4293fb856b46234934f73c50e21e17e0
greatCloud13 <138741201+greatCloud13@users.noreply.github.com>
2026-08-11 03:01:03 +0900

    FEAT: 누락된 ProblemDetailDTO 추가

---
+++ src/main/java/com/project/greatcloud13/ClimbingWith/dto/ProblemDetailDTO.java
@@ -0,0 +1,54 @@
+package com.project.greatcloud13.ClimbingWith.dto;
+
+import com.project.greatcloud13.ClimbingWith.entity.Problem;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ProblemDetailDTO {
+
+    private Long id;
+
+    private Long settingId;
+
+    private String title;
+
+    private String problemType;
+
+    private String gymLevel;
+
+    private Long levelId;
+
+    private String colorCode;
+
+    private Integer holdCount;
+
+    private String description;
+
+    private Float evaluation;
+
+    private Long tryCount;
+
+    private Long clearCount;
+
+    private Integer myBestDropPoint;
+
+    public static ProblemDetailDTO from(Problem problem, long tryCount, long clearCount, Integer myBestDropPoint){
+        return ProblemDetailDTO.builder()
+                .id(problem.getId())
+                .settingId(problem.getSetting().getId())
+                .title(problem.getTitle())
+                .problemType(problem.getProblemType().toString())
+                .levelId(problem.getGymLevel().getId())
+                .gymLevel(problem.getGymLevel().getLevelName())
+                .colorCode(problem.getGymLevel().getColorCode())
+                .holdCount(problem.getHoldCount())
+                .description(problem.getDescription())
+                .evaluation(problem.getEvaluation())
+                .tryCount(tryCount)
+                .clearCount(clearCount)
+                .myBestDropPoint(myBestDropPoint)
+                .build();
+    }
+}
