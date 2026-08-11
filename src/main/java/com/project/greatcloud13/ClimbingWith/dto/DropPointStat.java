16b64f2b2746f0bb9c80d3158d6cc678c5f4ef2f
greatCloud13 <138741201+greatCloud13@users.noreply.github.com>
2026-08-11 21:16:26 +0900

    FEAT: 누락 파일 추가 commit

---
+++ src/main/java/com/project/greatcloud13/ClimbingWith/dto/DropPointStat.java
@@ -0,0 +1,13 @@
+package com.project.greatcloud13.ClimbingWith.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class DropPointStat {
+
+    private Integer dropPoint;
+
+    private Long count;
+}
